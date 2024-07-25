7d34988f4fa19d6b3933a5791b45569e885a5331
f957-cua <ivanfiliptschik00@gmail.com>
2024-07-25 23:00:05 +0300

    browser routes old version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Routes, Route } from 'react-router-dom';
+import Menu from './pages/Menu/Menu';
+import Cart from './pages/Cart/Cart';
+import Error from './pages/Error/Error';
 
 function App() {
 
@@ -7,7 +11,12 @@ function App() {
     <>
       <Button>Click</Button>
       <Button appearence='big'>Click</Button>
-      <Input placeholder='Email'/>
+      <Input placeholder='Email' />
+      <Routes>
+        <Route path='/' element={<Menu />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </>
   )
 }
