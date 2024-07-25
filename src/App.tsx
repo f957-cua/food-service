9aa8a43579968f6e6393ce1465d9461231fae751
f957-cua <ivanfiliptschik00@gmail.com>
2024-07-25 23:54:54 +0300

    recommended browser router implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import Button from './components/Button/Button';
-import Input from './components/Input/Input';
-import { Routes, Route } from 'react-router-dom';
-import Menu from './pages/Menu/Menu';
-import Cart from './pages/Cart/Cart';
-import Error from './pages/Error/Error';
+import Input from './components/Input/Input'
+import Layout from './layouts/Menu/Menu';
+
 
 function App() {
 
@@ -12,11 +10,7 @@ function App() {
       <Button>Click</Button>
       <Button appearence='big'>Click</Button>
       <Input placeholder='Email' />
-      <Routes>
-        <Route path='/' element={<Menu />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
+      <Layout />
     </>
   )
 }
